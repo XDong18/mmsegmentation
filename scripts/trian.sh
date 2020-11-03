aadaf958f177656708fdd7ae990b27b7751bbbcf
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-03 17:06:14 +0800

    cc

--- scripts/trian.sh
+++ scripts/trian.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-PORT=29501 ./tools/dist_train.sh configs/fcn/fcn_r50-d8_512x512_20k_bdd100k.py 4 --work-dir out/fcn_r50-d8_512x512_20k
+PORT=29503 ./tools/dist_train.sh configs/fcn/fcn_r50-d8_512x512_20k_bdd100k.py 4 --work-dir out/fcn_r50-d8_512x512_20k
