d4247ef1577c1f235ee41957037934d1d652aa08
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-09 02:22:51 +0800

    cc

--- scripts/trian.sh
+++ scripts/trian.sh
@@ -1,2 +1,2 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-PORT=29503 ./tools/dist_train.sh configs/fcn/fcn_r50-d8_769x769_40k_bdd100k.py 4 --work-dir out/fcn_r50-d8_769x769_40k
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+PORT=29503 ./tools/dist_train.sh configs/deeplabv3plus/deeplabv3plus_r50-d8_769x769_40k_bdd100k.py 4 --work-dir out/deeplabv3plus_r50-d8_769x769_40k
