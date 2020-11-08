293506b696dc254526d2e6e6ac74cd2bcb7e2f30
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-09 02:17:00 +0800

    cc

--- mmseg/datasets/bdd100k.py
+++ mmseg/datasets/bdd100k.py
@@ -12,13 +12,18 @@ class Bdd100kDataset(CustomDataset):
         split (str): Split txt file for Pascal VOC.
     """
 
-    CLASSES = ('background', 'person', 'rider', 'car', 'bus', 'truck', 'bike', 'motor', 'traffic light', 'traffic sign', 'train')
+    CLASSES = ('road', 'sidewalk', 'building', 'wall', 'fence', 'pole',
+               'traffic light', 'traffic sign', 'vegetation', 'terrain', 'sky',
+               'person', 'rider', 'car', 'truck', 'bus', 'train', 'motorcycle',
+               'bicycle')
 
-    PALETTE = [[0, 0, 0], [128, 0, 0], [0, 128, 0], [128, 128, 0], [0, 0, 128],
-               [128, 0, 128], [0, 128, 128], [128, 128, 128], [64, 0, 0],
-               [192, 0, 0], [64, 128, 0]]
+    PALETTE = [[128, 64, 128], [244, 35, 232], [70, 70, 70], [102, 102, 156],
+               [190, 153, 153], [153, 153, 153], [250, 170, 30], [220, 220, 0],
+               [107, 142, 35], [152, 251, 152], [70, 130, 180], [220, 20, 60],
+               [255, 0, 0], [0, 0, 142], [0, 0, 70], [0, 60, 100],
+               [0, 80, 100], [0, 0, 230], [119, 11, 32]]
 
     def __init__(self, split, **kwargs):
         super(Bdd100kDataset, self).__init__(
-            img_suffix='.jpg', seg_map_suffix='.png', split=split, **kwargs)
+            img_suffix='.jpg', seg_map_suffix='_train_id.png', split=split, **kwargs)
         assert osp.exists(self.img_dir)
--- scripts/trian.sh
+++ scripts/trian.sh
@@ -1,3 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-PORT=29503 ./tools/dist_train.sh configs/fcn/fcn_r50-d8_512x512_20k_bdd100k.py 4 --work-dir out/fcn_r50-d8_512x512_20k \
---resume-from ./out/fcn_r50-d8_512x512_20k/latest.pth
+PORT=29503 ./tools/dist_train.sh configs/fcn/fcn_r50-d8_769x769_40k_bdd100k.py 4 --work-dir out/fcn_r50-d8_769x769_40k
