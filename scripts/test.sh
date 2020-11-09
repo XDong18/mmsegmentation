17efcb8ff81d893a153c61d94ef96c681f0aee0d
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-10 00:02:07 +0800

    cc

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
-python tools/test.py configs/fcn/fcn_r50-d8_512x512_20k_bdd100k.py \
-    out/fcn_r50-d8_512x512_20k/latest.pth \
-    --show-dir show/fcn_r50-d8_512x512_20k
+python tools/test.py configs/fcn/fcn_r50-d8_769x769_40k_bdd100k.py \
+    out/fcn_r50-d8_769x769_40k/latest.pth \
+    --show-dir show/fcn_r50-d8_769x769_40k
