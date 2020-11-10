3799af8f40e629027cf3679540d31b7f3a9f1737
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-11 07:42:21 +0800

    cc

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=6,7,8,9
-PORT=29503 ./tools/dist_test.sh configs/danet/danet_r50-d8_769x769_40k_bdd100k.py \
-    b_out/danet_r50-d8_769x769_40k/latest.pth \
+PORT=29503 ./tools/dist_test.sh configs/emanet/emanet_r50-d8_769x769_80k_bdd100k.py \
+    c_out/emanet_r50-d8_769x769_80k/latest.pth \
     4 --eval mAP
