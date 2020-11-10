9a4168c163d77573db12560e07d084664be4c629
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-11 07:26:41 +0800

    cc

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,4 @@
-python tools/test.py configs/fcn/fcn_r50-d8_769x769_40k_bdd100k.py \
+export CUDA_VISIBLE_DEVICES=6,7,8,9
+PORT=29503 ./tools/dist_test.sh configs/fcn/fcn_r50-d8_769x769_40k_bdd100k.py \
     out/fcn_r50-d8_769x769_40k/latest.pth \
-    --show-dir show/fcn_r50-d8_769x769_40k
+    4 --eval mAP
