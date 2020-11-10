fce07ad0baca089d9449e2cb51c3a61617ef9e26
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-10 21:29:42 +0800

    cc

--- scripts/trian.sh
+++ scripts/trian.sh
@@ -1,2 +1,3 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-PORT=29502 ./tools/dist_train.sh configs/pspnet/pspnet_r50-d8_769x769_40k_bdd100k.py 4 --work-dir out/pspnet_r50-d8_769x769_40k
+PORT=29502 ./tools/dist_train.sh configs/pspnet/pspnet_r50-d8_769x769_40k_bdd100k.py 4 --work-dir /shared/xudongliu/code/f_server/mmsegmentation/out/pspnet_r50-d8_769x769_40k \
+--resume-from /shared/xudongliu/code/f_server/mmsegmentation/out/pspnet_r50-d8_769x769_40k/latest.pth
