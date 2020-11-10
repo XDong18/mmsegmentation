b96fc0dd0c06eaece811f630f1bc79dca896d845
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-11 07:40:48 +0800

    cc

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=6,7,8,9
-PORT=29503 ./tools/dist_test.sh configs/pspnet/pspnet_r50-d8_769x769_40k_bdd100k.py \
-    f_out/pspnet_r50-d8_769x769_40k/latest.pth \
+PORT=29503 ./tools/dist_test.sh configs/danet/danet_r50-d8_769x769_40k_bdd100k.py \
+    b_out/danet_r50-d8_769x769_40k/latest.pth \
     4 --eval mAP
