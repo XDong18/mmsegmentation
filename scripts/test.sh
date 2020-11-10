66d21416c576c5f13cadc212bc0676cefd1eecf0
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-11 07:35:41 +0800

    cc

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=6,7,8,9
-PORT=29503 ./tools/dist_test.sh configs/deeplabv3plus/deeplabv3plus_r50-d8_769x769_40k_bdd100k.py \
-    f_out/deeplabv3plus_r50-d8_769x769_40k/latest.pth \
+PORT=29503 ./tools/dist_test.sh configs/pspnet/pspnet_r50-d8_769x769_40k_bdd100k.py \
+    f_out/pspnet_r50-d8_769x769_40k/latest.pth \
     4 --eval mAP
