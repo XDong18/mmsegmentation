402704dd868130428247e15bb04ed23c243f8d87
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-12-09 16:04:28 +0800

    cc

---
+++ auto_html.py
@@ -0,0 +1,19 @@
+import os
+
+img_dir = 'show/deeplabv3plus_r50-d8_769x769_40k'
+img_list = sorted(os.listdir(img_dir))
+out_fn = 'show/deeplabv3plus_r50-d8_769x769_40k_val'
+with open (out_fn, 'w') as f:
+    f.write('<html>\n')
+    f.write('<body>\n')
+    f.write('<h1>deeplabv3plus_r50-d8_769x769_40k_val</h1>\n')
+
+    for i, img in enumerate(img_list):
+        name = "<p>" + str(i) + ' ---- ' + img + "</p>\n"
+        f.write(name)
+        new_line = "<img src='" + os.path.join('deeplabv3plus_r50-d8_769x769_40k', img) + "'>\n"
+        f.write(new_line)
+
+    # f.write("<img src='" + "test.jpg" + "'>\n")
+    f.write('</body>\n')
+    f.write('</html>\n')
