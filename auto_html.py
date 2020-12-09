a411517803e43ed7b4d1a044337d3f45eb0f3837
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-12-09 16:24:26 +0800

    cc

--- auto_html.py
+++ auto_html.py
@@ -1,17 +1,17 @@
 import os
 
-img_dir = 'show/deeplabv3plus_r50-d8_769x769_40k'
+img_dir = 'show/deeplabv3plus_r50-d8_769x769_40k_torchvision'
 img_list = sorted(os.listdir(img_dir))
-out_fn = 'show/deeplabv3plus_r50-d8_769x769_40k_val.html'
+out_fn = 'show/deeplabv3plus_r50-d8_769x769_40k_torchvision_val.html'
 with open (out_fn, 'w') as f:
     f.write('<html>\n')
     f.write('<body>\n')
-    f.write('<h1>deeplabv3plus_r50-d8_769x769_40k_val</h1>\n')
+    f.write('<h1>deeplabv3plus_r50-d8_769x769_40k_torchvision_val</h1>\n')
 
     for i, img in enumerate(img_list):
         name = "<p>" + str(i) + ' ---- ' + img + "</p>\n"
         f.write(name)
-        new_line = "<img src='" + os.path.join('deeplabv3plus_r50-d8_769x769_40k', img) + "'>\n"
+        new_line = "<img src='" + os.path.join('deeplabv3plus_r50-d8_769x769_40k_torchvision', img) + "'>\n"
         f.write(new_line)
 
     # f.write("<img src='" + "test.jpg" + "'>\n")
