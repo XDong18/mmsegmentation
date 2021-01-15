b0bdd71fbe1f5d19a76e47d3918fb34ceddebcb3
XDong18 <liuxd1215@gmail.com>
2021-01-15 08:09:26 +0800

    cc

--- scripts/trian.sh
+++ scripts/trian.sh
@@ -1,2 +1,3 @@
-export CUDA_VISIBLE_DEVICES=1,2,3,4
-PORT=29502 ./tools/dist_train.sh configs/deeplabv3plus/deeplabv3plus_r50-d8_769x769_80k_bdd100k.py 4 --work-dir ./out/deeplabv3plus_r50-d8_769x769_80k_bdd100k/
+export CUDA_VISIBLE_DEVICES=6,7,8,9
+PORT=29502 ./tools/dist_train.sh configs/deeplabv3plus/deeplabv3plus_r50-d8_769x769_80k_bdd100k.py 4 --work-dir ./out/deeplabv3plus_r50-d8_769x769_80k_bdd100k/ \
+--resume-from ./out/deeplabv3plus_r50-d8_769x769_80k_bdd100k/latest.pth
