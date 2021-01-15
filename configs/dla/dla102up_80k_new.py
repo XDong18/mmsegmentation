4c6a11da80eef1930f186f48669b1eef930afcdb
XDong18 <liuxd1215@gmail.com>
2021-01-15 20:53:00 +0800

    cc

--- configs/dla/dla102up_80k_new.py
+++ configs/dla/dla102up_80k_new.py
@@ -51,3 +51,17 @@ lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
 runner = dict(type='IterBasedRunner', max_iters=80000)
 checkpoint_config = dict(by_epoch=False, interval=4000)
 evaluation = dict(interval=4000, metric='mIoU')
+
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook', by_epoch=False),
+        # dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+cudnn_benchmark = True
