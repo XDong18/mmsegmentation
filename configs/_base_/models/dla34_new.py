9b145171460f61d7645fda52341263d0319c9553
XDong18 <liuxd1215@gmail.com>
2021-01-18 05:04:03 +0800

    cc

---
+++ configs/_base_/models/dla34_new.py
@@ -0,0 +1,35 @@
+# model settings
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+model = dict(
+    type='EncoderDecoder',
+    pretrained='/shared/xudongliu/models/dla34-24a49e58.pth',
+    backbone=dict(
+        type='DLA',
+        levels=[1, 1, 1, 2, 2, 1],
+        channels=[16, 32, 64, 128, 256, 512],
+        block_num=2,
+        num_classes=19,
+        return_levels=True,
+        norm_cfg=norm_cfg
+        ),
+    neck=dict(
+        type='DLAUp',
+        channels=[32, 64, 128, 256, 512],
+        scales=(1, 2, 4, 8, 16),
+        norm_cfg=norm_cfg
+        ),
+    decode_head=dict(
+            type='DLAsHead',
+            channels=32,
+            in_channels=32,
+            num_classes=19,
+            dropout_ratio=0,
+            loss_decode=dict(
+                     type='CrossEntropyLoss',
+                     use_sigmoid=False,
+                     loss_weight=1.0),
+            )
+    )
+# model training and testing settings
+train_cfg = dict()
+test_cfg = dict(mode='whole')
--- scripts/trian.sh
+++ scripts/trian.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=2,3,4,5
-PORT=29502 ./tools/dist_train.sh configs/dla/dla102up_80k_mm.py 4 --work-dir ./out/dla102up_80k_mm/
+PORT=29502 ./tools/dist_train.sh configs/dla/dla34up_80k_new_sbn.py 4 --work-dir ./out/dla34up_80k_new_sbn/
