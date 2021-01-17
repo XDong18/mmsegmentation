e15d20b4cd20c52584dba1b9a7844c3195e78069
XDong18 <liuxd1215@gmail.com>
2021-01-18 05:28:32 +0800

    cc

--- scripts/trian.sh
+++ scripts/trian.sh
@@ -1,2 +1,2 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3,6,7,8,9
-PORT=29502 ./tools/dist_train.sh configs/dla/dla102up_80k_new.py 8 --work-dir ./out/dla102up_80k_new_bs16/
+export CUDA_VISIBLE_DEVICES=6,7,8,9
+PORT=29502 ./tools/dist_train.sh configs/emanet/emanet_r101-d8_769x769_80k_bdd100k.py 4 --work-dir ./out/emanet_r101-d8_769x769_80k_bdd100k/
