2f3d43aa3d6bec80d712683303922e90fce6ffa8
XDong18 <liuxd1215@gmail.com>
2021-01-18 02:11:25 +0800

    cc

--- configs/dla/dla102up_80k_mm.py
+++ configs/dla/dla102up_80k_mm.py
@@ -2,5 +2,5 @@ _base_ = [
     '../_base_/models/dla102up.py', '../_base_/datasets/bdd100k.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
 ]
-
+crop_size = (768, 768)
 test_cfg = dict(mode='slide', crop_size=(769, 769), stride=(513, 513))
