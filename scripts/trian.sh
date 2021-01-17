5b8be511e5b1bdcf853b2091549fa35675c3248f
XDong18 <liuxd1215@gmail.com>
2021-01-17 08:12:39 +0800

    cc

--- scripts/trian.sh
+++ scripts/trian.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=6,7,8,9
-PORT=29502 ./tools/dist_train.sh configs/dla/dla102up_80k_new.py 4 --work-dir ./out/dla102up_80k_new_strict/
+PORT=29502 ./tools/dist_train.sh configs/dla/dla102up_80k_new.py 4 --work-dir ./out/dla102up_80k_new_sbn/
