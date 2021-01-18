91b3469ae0b43c22547e7e0f6a6e6acaacfb5400
XDong18 <liuxd1215@gmail.com>
2021-01-18 16:50:54 +0800

    cc

--- auto_html.py
+++ auto_html.py
@@ -1,17 +1,17 @@
 import os
 
-img_dir = 'show/deeplabv3plus_r50-d8_769x769_40k_torchvision'
+img_dir = 'show/dla34up_80k_new_sbn'
 img_list = sorted(os.listdir(img_dir))
-out_fn = 'show/deeplabv3plus_r50-d8_769x769_40k_torchvision_val.html'
+out_fn = 'show/dla34up_80k_new_sbn.html'
 with open (out_fn, 'w') as f:
     f.write('<html>\n')
     f.write('<body>\n')
-    f.write('<h1>deeplabv3plus_r50-d8_769x769_40k_torchvision_val</h1>\n')
+    f.write('<h1>dla34up_80k_new_sbn</h1>\n')
 
     for i, img in enumerate(img_list):
         name = "<p>" + str(i) + ' ---- ' + img + "</p>\n"
         f.write(name)
-        new_line = "<img src='" + os.path.join('deeplabv3plus_r50-d8_769x769_40k_torchvision', img) + "'>\n"
+        new_line = "<img src='" + os.path.join('dla34up_80k_new_sbn', img) + "'>\n"
         f.write(new_line)
 
     # f.write("<img src='" + "test.jpg" + "'>\n")
