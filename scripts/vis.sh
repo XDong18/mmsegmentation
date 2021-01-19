2f3f790bd648dee6d46b5ed2a16fcad28a150a90
XDong18 <liuxd1215@gmail.com>
2021-01-19 18:58:23 +0800

    cc

--- scripts/vis.sh
+++ scripts/vis.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=6,7,8,9
-python tools/test.py configs/dla/dla34up_80k_new_sbn.py ./out/dla_model/dla34_bs16_500e_new_pretrained.pth --eval mIoU
+python tools/test.py configs/dla/dla34up_80k_new_sbn.py ./out/dla_model/dla34_bs16_500e.pth --eval mIoU
