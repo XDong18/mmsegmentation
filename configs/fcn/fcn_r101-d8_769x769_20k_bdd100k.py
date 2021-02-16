b10408113377914c48d7ef0409af8fec26e38cd2
XDong18 <liuxd1215@gmail.com>
2021-02-16 10:09:28 +0800

    cc

---
+++ configs/fcn/fcn_r101-d8_769x769_20k_bdd100k.py
@@ -0,0 +1,17 @@
+_base_ = [
+    '../_base_/models/fcn_r50-d8.py', '../_base_/datasets/bdd100k.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
+]
+
+model = dict(
+    pretrained='open-mmlab://resnet101_v1c',
+    backbone=dict(depth=101),
+    decode_head=dict(align_corners=True),
+    auxiliary_head=dict(align_corners=True))
+
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2
+)
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0005)
+test_cfg = dict(mode='slide', crop_size=(769, 769), stride=(513, 513))
