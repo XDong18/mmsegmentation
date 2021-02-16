ace93c09ea755065853d60390c522afaeb28cd23
XDong18 <liuxd1215@gmail.com>
2021-02-16 20:35:33 +0800

    cc

---
+++ configs/deeplabv3plus/deeplabv3plus_r101-d8_769x769_20k_bdd100k.py
@@ -0,0 +1,16 @@
+_base_ = [
+    '../_base_/models/deeplabv3plus_r50-d8.py', '../_base_/datasets/bdd100k.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
+]
+model = dict(
+    decode_head=dict(align_corners=True),
+    auxiliary_head=dict(align_corners=True),
+    pretrained='open-mmlab://resnet101_v1c',
+    backbone=dict(depth=101))
+
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2
+)
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0005)
+test_cfg = dict(mode='slide', crop_size=(769, 769), stride=(513, 513))
