11ee7963ecfb725b61cd20e4252fb8f33d7fad6e
XDong18 <liuxd1215@gmail.com>
2021-02-18 19:52:31 +0800

    cc

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 PORT=29507 ./tools/dist_train.sh \
-configs/emanet/emanet_r101-d8_769x769_40k_bdd100k.py 8 \
---work-dir ./out/emanet_r101-d8_769x769_40k_bdd100k/
+configs/pspnet/pspnet_r101-d8_769x769_40k_bdd100k.py 8 \
+--work-dir ./out/pspnet_r101-d8_769x769_40k_bs16_bdd100k/
