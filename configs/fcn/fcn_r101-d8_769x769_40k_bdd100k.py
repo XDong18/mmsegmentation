38641eed0acbea0cb65c7bd3a42830fae1098fae
XDong18 <liuxd1215@gmail.com>
2021-02-19 19:48:54 +0800

    cc

--- configs/fcn/fcn_r101-d8_769x769_40k_bdd100k.py
+++ configs/fcn/fcn_r101-d8_769x769_40k_bdd100k.py
@@ -13,4 +13,5 @@ data = dict(
     samples_per_gpu=2,
     workers_per_gpu=2
 )
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0005)
 test_cfg = dict(mode='slide', crop_size=(769, 769), stride=(513, 513))
