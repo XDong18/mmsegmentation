bd7676cafa5f47bb2df4db098953c90ed7fb0840
XDong18 <liuxd1215@gmail.com>
2021-02-22 03:27:12 +0800

    cc

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 PORT=29507 ./tools/dist_train.sh \
-configs/deeplabv3plus/deeplabv3plus_r101-d8_769x769_40k_bdd100k.py 8 \
---work-dir ./out/deeplabv3plus_r101-d8_769x769_40k_bs16_bdd100k
+configs/danet/danet_r101-d8_769x769_40k_bdd100k.py 8 \
+--work-dir ./out/danet_r101-d8_769x769_40k_bs16_bdd100k
