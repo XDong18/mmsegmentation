5b6c7bb760ef8f586070a3629e3ba9e0ddd97acc
XDong18 <liuxd1215@gmail.com>
2021-03-09 13:58:48 +0800

    update

--- scripts/vis.sh
+++ scripts/vis.sh
@@ -1,3 +1,9 @@
 export CUDA_VISIBLE_DEVICES=4,5
 python tools/test.py configs/danet/danet_r101-d8_769x769_40k_bdd100k.py ./out/danet_r101-d8_769x769_40k_bs16_bdd100k/latest.pth \
 --out ./result/test/danet_r101-d8_769x769_40k_bdd100k.pkl --show-dir ./show/test/danet_r101-d8_769x769_40k_bdd100k
+
+python tools/test.py configs/emanet/emanet_r101-d8_769x769_40k_bdd100k.py.py ./out/emanet_r101-d8_769x769_40k_bdd100k/latest.pth \
+--out ./result/test/emanet_r101-d8_769x769_40k_bdd100k.pkl --show-dir ./show/test/emanet_r101-d8_769x769_40k_bdd100k
+
+python tools/test.py configs/pspnet/pspnet_r101-d8_769x769_40k_bdd100k.py ./out/pspnet_r101-d8_769x769_40k_bs16_bdd100k/latest.pth \
+--out ./result/test/pspnet_r101-d8_769x769_40k_bdd100k.pkl --show-dir ./show/test/pspnet_r101-d8_769x769_40k_bdd100k
