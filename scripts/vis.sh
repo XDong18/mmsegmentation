87116f54c166590f2ab17223630a3e1a41d8ce60
XDong18 <liuxd1215@gmail.com>
2021-03-09 10:58:59 +0800

    update

--- scripts/vis.sh
+++ scripts/vis.sh
@@ -1,3 +1,3 @@
 export CUDA_VISIBLE_DEVICES=4,5
 python tools/test.py configs/fcn/fcn_r101-d8_769x769_40k_bdd100k.py ./out/fcn_r101-d8_769x769_40k_bs16_bdd100k/latest.pth \
---show-dir ./show/fcn_r101-d8_769x769_40k_bs16_bdd100k
+--out ./result/fcn_r101-d8_769x769_40k_bs16_bdd100k.pkl --show-dir ./show/fcn_r101-d8_769x769_40k_bs16_bdd100k
