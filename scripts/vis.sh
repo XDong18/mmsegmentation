eb35a92cb26b887f4d58e235536e1c110f30938a
XDong18 <liuxd1215@gmail.com>
2021-03-09 10:56:01 +0800

    update

--- scripts/vis.sh
+++ scripts/vis.sh
@@ -1,3 +1,3 @@
 export CUDA_VISIBLE_DEVICES=4,5
-PORT=25003 tools/dist_test.sh configs/fcn/fcn_r101-d8_769x769_40k_bdd100k.py ./out/fcn_r101-d8_769x769_40k_bs16_bdd100k/latest.pth \
---eval mIoU --show-dir ./show/fcn_r101-d8_769x769_40k_bs16_bdd100k
+python tools/test.py configs/fcn/fcn_r101-d8_769x769_40k_bdd100k.py ./out/fcn_r101-d8_769x769_40k_bs16_bdd100k/latest.pth \
+--show-dir ./show/fcn_r101-d8_769x769_40k_bs16_bdd100k
