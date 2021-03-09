b7b991b5f089ce7cc6f33d41cff480178da15b23
XDong18 <liuxd1215@gmail.com>
2021-03-09 13:53:57 +0800

    update

--- scripts/vis.sh
+++ scripts/vis.sh
@@ -1,3 +1,3 @@
 export CUDA_VISIBLE_DEVICES=4,5
-python tools/test.py configs/deeplabv3plus/deeplabv3plus_r101-d8_769x769_40k_bdd100k.py ./out/deeplabv3plus_r101-d8_769x769_40k_bs16_bdd100k/latest.pth \
---out ./result/test/deeplabv3plus_r101-d8_769x769_40k_bs16_bdd100k.pkl --show-dir ./show/test/deeplabv3plus_r101-d8_769x769_40k_bs16_bdd100k
+python tools/test.py configs/danet/danet_r101-d8_769x769_40k_bdd100k.py ./out/danet_r101-d8_769x769_40k_bs16_bdd100k/latest.pth \
+--out ./result/test/danet_r101-d8_769x769_40k_bdd100k.pkl --show-dir ./show/test/danet_r101-d8_769x769_40k_bdd100k
