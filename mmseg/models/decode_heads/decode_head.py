911065b0c6bc0a161fcd1dec42db24976e43d5fd
XDong18 <liuxd1215@gmail.com>
2021-03-16 23:32:16 +0800

    update

--- mmseg/models/decode_heads/decode_head.py
+++ mmseg/models/decode_heads/decode_head.py
@@ -224,7 +224,7 @@ class BaseDecodeHead(nn.Module, metaclass=ABCMeta):
         else:
             seg_weight = None
         seg_label = seg_label.squeeze(1)
-        print('\npin'self.num_classes, seg_label.max(), 'pin\n')
+        print('\npin', self.num_classes, seg_label.max(), 'pin\n')
         loss['loss_seg'] = self.loss_decode(
             seg_logit,
             seg_label,
