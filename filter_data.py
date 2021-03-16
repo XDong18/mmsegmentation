2d886a61db387ca48373cdb4b05a0ec9a75a8606
XDong18 <liuxd1215@gmail.com>
2021-03-16 22:24:28 +0800

    update

--- filter_data.py
+++ filter_data.py
@@ -5,12 +5,13 @@ img_dir = '/shared/xudongliu/bdd100k/100k/train'
 label_dir = '/shared/xudongliu/bdd100k/labels/lane/lane_train'
 temp_img_dir  = '/shared/xudongliu/bdd100k/100k/train_else/'
 label_names = os.listdir(label_dir)
-
+num  = 0
 for fn in os.listdir(img_dir):
     name = fn.split('.')[0]+'.png'
     if name not in label_names:
         cmd = 'mv ' + osp.join(img_dir, fn) + ' ' + temp_img_dir
-        print('caught ', fn)
+        num += 1
+        print('caught ', fn, num)
         # exit()
     
 
