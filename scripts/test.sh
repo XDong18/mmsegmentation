13c16d646dadc16507391e4637f049f2cb8489d9
XDong18 <liuxd1215@gmail.com>
2021-03-22 23:16:34 +0800

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,4 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 PORT=29503 ./tools/dist_test.sh \
-configs/dla_lane/dla34up_lane_12e.py 4
+configs/dla_lane/dla34up_lane_12e.py \
+out/dla34up_lane_12e_iter/latest.pth 4
