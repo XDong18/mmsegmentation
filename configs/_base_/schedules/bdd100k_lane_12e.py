14c9a6d101b648aac13a10b3fce6515d77f3db46
XDong18 <liuxd1215@gmail.com>
2021-03-22 22:41:34 +0800

    update

--- configs/_base_/schedules/bdd100k_lane_12e.py
+++ configs/_base_/schedules/bdd100k_lane_12e.py
@@ -1,5 +1,5 @@
 # optimizer
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0005)
+optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0005)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
