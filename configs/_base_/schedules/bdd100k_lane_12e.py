39478ddaa186e28d9ab85e7421b3709dccfeb6c1
XDong18 <liuxd1215@gmail.com>
2021-03-22 22:10:34 +0800

    update

--- configs/_base_/schedules/bdd100k_lane_12e.py
+++ configs/_base_/schedules/bdd100k_lane_12e.py
@@ -4,6 +4,6 @@ optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
 # runtime settings
-runner = dict(type='IterBasedRunner', max_epochs=103824)
+runner = dict(type='IterBasedRunner', max_iters=103824)
 checkpoint_config = dict(by_epoch=False, interval=2000)
 evaluation = dict(interval=100, metric='mIoU')
